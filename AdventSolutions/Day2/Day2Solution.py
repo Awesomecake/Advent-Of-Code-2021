a231ea1d39b29a494e5307ea9e67a72bbcffcc23
Aaron Bush <aaronodellbush@gmail.com>
2022-12-07 16:36:16 -0500

    Day 2

--- AdventSolutions/Day2/Day2Solution.py
+++ AdventSolutions/Day2/Day2Solution.py
@@ -1,12 +1,21 @@
-location = 'AdventSolutions/Day1/AdventData.txt'
+location = 'AdventSolutions/Day2/AdventData.txt'
 
 def Day2Star1():
     with open(location) as data_file:
-        lines = [int(line.strip()) for line in data_file]
+        lines = [{'dir': line.split()[0],'dist': int(line.split()[1])} for line in data_file]
+        horizontal = depth = 0
         for i in range(len(lines)):
+            if(lines[i]['dir'] == 'forward'): horizontal += lines[i]['dist']
+            if(lines[i]['dir'] == 'up'): depth -= lines[i]['dist']
+            if(lines[i]['dir'] == 'down'): depth += lines[i]['dist']
+        print(f"Day 2 Star 1 Answer: {depth*horizontal}")
 
 def Day2Star2():
     with open(location) as data_file:
-        lines = [int(line.strip()) for line in data_file]
+        lines = [{'dir': line.split()[0],'dist': int(line.split()[1])} for line in data_file]
+        horizontal = depth = aim = 0
         for i in range(len(lines)):
-            
+            if(lines[i]['dir'] == 'forward'): horizontal += lines[i]['dist']; depth += aim * lines[i]['dist']
+            if(lines[i]['dir'] == 'up'): aim -= lines[i]['dist']
+            if(lines[i]['dir'] == 'down'): aim += lines[i]['dist']
+        print(f"Day 2 Star 2 Answer: {depth*horizontal}\n")
--- Advent_Of_Code_2021.py
+++ Advent_Of_Code_2021.py
@@ -1,4 +1,8 @@
 from AdventSolutions.Day1.Day1Solution import Day1Star1, Day1Star2
+from AdventSolutions.Day2.Day2Solution import Day2Star1, Day2Star2
 
 Day1Star1()
 Day1Star2()
+
+Day2Star1()
+Day2Star2()
